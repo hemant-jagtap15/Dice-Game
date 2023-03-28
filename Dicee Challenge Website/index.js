d3f0f4ca68a0eb7348e887939addfb9f1f1a83f7
hemant-jagtap15 <115880007+hemant-jagtap15@users.noreply.github.com>
2023-03-28 12:12:43 +0530

    dicegame files

---
+++ Dicee Challenge Website/index.js
@@ -0,0 +1,21 @@
+// for dice 1;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "dice" + randomNumber1 + ".png";// mean dice1.png - dice2.png;
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images.dice6.png;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+//for dice2 
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImageSource2);
+
+if(randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩Player1 Wins!";
+} 
+else if(randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player2 Wins!🚩";
+} else {
+    document.querySelector("h1").innerHTML = "Draw";
+}
